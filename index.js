553d0e93571ca44928ef66b0c596297e9f5b36e5
JooEun-Jeong <prichard147@naver.com>
2022-09-01 17:03:29 +0900

    unify DB + data extraction

--- index.js
+++ index.js
@@ -2,17 +2,38 @@ const express = require("express"); // express module을 가져온다
 const app = express(); // 모듈을 이용해 새 앱을 만든다.
 const port = 5000;
 const config = require("./config/key");
-const qs = require("query-string");
 const bodyParser = require("body-parser");
-const cors = require('cors')
+const MongoClient = require("mongodb").MongoClient;
+const cors = require("cors");
+const https = require("https");
+const { syncBuiltinESMExports } = require("module");
 // const { Ddata } = require("./models/Ddata");
 
+const DATABASE_NAME = "testdb";
+let database, collection;
+
 // application/x-www-form-urlencoded 형식으로 된 데이터를 분석해서 가져올 수 있게 해줌
-app.use(bodyParser.urlencoded({ extended: true })); //클에서오는 정보를 서버에서 분석해서 가져올 수 있게 해줌
+app.use(bodyParser.urlencoded({ extended: true })); //클라이언트에서 오는 정보를 서버에서 분석해서 가져올 수 있게 해줌
 // applicaiton/json 형식의 데이터를 분석해서 가져올 수 있게 해줌
 app.use(bodyParser.json());
+app.use(cors());
 
-app.use(cors())
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+  // mongoDB 연결만
+  MongoClient.connect(
+    config.mongoURI,
+    { useNewUrlParser: true },
+    (error, client) => {
+      if (error) {
+        throw error;
+      }
+      database = client.db(DATABASE_NAME);
+      collection = database.collection("test01");
+      console.log("Connected to `" + DATABASE_NAME + "`!");
+    }
+  );
+});
 
 const spawn = require("child_process").spawn;
 
@@ -50,45 +71,104 @@ app.post("/api", (req, res) => {
   console.log(post);
   input = post.url; //url은 key의 이름임
 
-  const Url = input;
-  // const Url = "https://www.clinicaltrials.gov/ct2/show/NCT04577378";
-  console.log("#####", Url);
-  const python_result = spawn("/home/jun/anaconda3/bin/python", [
-    "./resource_control.py",
-    Url,
-  ]);
+  let Url = input;
+  console.log("#####", Url); // url
+  let NCTID;
 
-  let getJson;
-  let result_json;
-  python_result.stdout.on("data", (data) => {
-    console.log(`stdout: ${data.toString()}`);
-    getJson = data.toString();
-    getJson = getJson.replaceAll('\'', '\"');
-  
-    result_json = JSON.parse(getJson);
+  //NCT 번호를 뽑아내기 위한 작업
+  if (Url.includes("NCT") === true) {
+    //이미 URL이 NCT를 가지고 있는 경우
+    let Htmltext = Url;
+    let findtext = Htmltext.match("NCT[0-9]+"); //NCT를 찾아 번호를 뽑아낸다.
+    NCTID = findtext[0];
+  } else {
+    // NCT를 가지고 있지 않은 경우
+
+    // URL이 이미 json 형태인 경우
+    if (Url.includes("json") !== true) {
+      // URL이 json이 아닌 경우
+      let expr = Url.match("expr=[0-9a-zA-Z%+.]+")[0];
+      // console.log("expr", expr);
+      Url =
+        "https://clinicaltrials.gov/api/query/full_studies?" +
+        expr +
+        "&fmt=json";
+      console.log(Url);
+    }
+
+    https.get(Url, (res) => {
+      let rawHtml = "";
+      res.on("data", (chunk) => {
+        rawHtml += chunk;
+      });
+      res.on("end", () => {
+        try {
+          Htmltext = JSON.parse(rawHtml);
+          NCTID =
+            Htmltext.FullStudiesResponse.FullStudies[0].Study.ProtocolSection
+              .IdentificationModule.NCTId;
+        } catch (e) {
+          console.error(e.message);
+        }
+      });
+    });
+  }
+
+  //MongoDB에서 가져옴
+  let isSuccess = false;
+  let query = { _id: "NCT01967771" };
+  // console.log("mongo 진입 전");
+  collection.findOne(query, (error, result) => {
+    if (error) {
+      console.log("findOne's error not empty result: ", error);
+    } else {
+      console.log("hello");
+      if (result !== null) {
+        console.log("MongoDB\n");
+        isSuccess = true;
+        console.log(result);
+        res.json(result);
+      }
+    }
   });
 
-  python_result.stderr.on("data", (data) => {
-    console.error(`stderr: ${data.toString()}`);
-  });
+  if (isSuccess === false) {
+    // resource_control 실시간으로 돌리기
+    const python_result = spawn("/home/jun/anaconda3/bin/python", [
+      "./resource_control.py",
+      Url,
+    ]);
 
-  python_result.on("close", (code) => {
-    console.log(`child process exited with code ${code}`);
-    // console.log('======', typeof result_json); // string
-    res.json(result_json);
-  
-    ///// 데이터 받은거 저장
-    // ddata.save((err, doc)=>{
-    //   if(err) return res.json({success: false, err});
-    //   return res.status(200).json({
-    //     success: true
-    //   });
-    // })
-    // res.send(ddata);
-    // return res.json(python_result);
-    // res.redirect('/')
-  });
+    let getJson;
+    let result_json;
+    python_result.stdout.on("data", (data) => {
+      console.log(`stdout: ${data.toString()}`);
+      getJson = data.toString();
+      getJson = getJson.replaceAll("'", '"');
 
+      result_json = JSON.parse(getJson);
+    });
+    python_result.stderr.on("data", (data) => {
+      console.error(`stderr: ${data.toString()}`);
+    });
+
+    python_result.on("close", (code) => {
+      console.log(`child process exited with code ${code}`);
+      // console.log('======', typeof result_json); // object
+      return res.json(result_json);
+
+      ///// 데이터 받은거 저장
+      // ddata.save((err, doc)=>{
+      //   if(err) return res.json({success: false, err});
+      //   return res.status(200).json({
+      //     success: true
+      //   });
+      // })
+      // res.send(ddata);
+      // return res.json(python_result);
+      // res.redirect('/')
+    });
+  }
 });
 
 app.get("/api/hello", (req, res) => {
@@ -96,6 +176,8 @@ app.get("/api/hello", (req, res) => {
   res.send("hi");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+
+function sleep(ms) {
+  const wakeUpTime = Date.now() + ms;
+  while (Date.now() < wakeUpTime) {}
+}
